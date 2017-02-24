ec8f920db7d21c8219d9c47c7dd8c66cce826ffb
Anibal <detops.tk@gmail.com>
2017-02-23 22:02:24 -0500

    update: change server

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '123456');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql://mysqlpepe:3306/');
+define('DB_HOST', 'http://mysqlpepe-acopitan.44fs.preview.openshiftapps.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
